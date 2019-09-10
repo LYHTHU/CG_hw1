90cdf37f4787ca53140d41f819b529ba951a3b24
yunhaoli <yunhaoli.cs@gmail.com>
2019-09-10 17:01:12 -0400

    Playing

---
+++ worlds/week1/shaders/fragment.frag.glsl
@@ -0,0 +1,28 @@
+#version 300 es
+precision highp float;
+
+uniform float uTime;   // TIME, IN SECONDS
+in vec3 vPos;     // -1 < vPos.x < +1
+// -1 < vPos.y < +1
+//      vPos.z == 0
+
+out vec4 fragColor; 
+ 
+void main() {
+
+  // HERE YOU CAN WRITE ANY CODE TO
+  // DEFINE A COLOR FOR THIS FRAGMENT
+  float x = cos(uTime);
+  float y = sin(uTime);
+  float t = x*vPos.x + y*vPos.y - 0.36;
+
+  float red = sin(5.*t);; 
+  float green = cos(5.*t);
+  float blue = t;
+
+  // R,G,B EACH RANGE FROM 0.0 TO 1.0  
+  vec3 color = vec3(red, green, blue);
+    
+  // THIS LINE OUTPUTS THE FRAGMENT COLOR
+  fragColor = vec4(sqrt(color), 1.0);
+}
