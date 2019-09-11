66bb84ae7c4be031b4b6a63fabf7e38b3926c4fc
LYHTHU <yunhaoli.cs@gmail.com>
2019-09-11 00:17:17 -0400

    update2

--- worlds/week1/shaders/fragment.frag.glsl
+++ worlds/week1/shaders/fragment.frag.glsl
@@ -12,13 +12,14 @@ void main() {
 
   // HERE YOU CAN WRITE ANY CODE TO
   // DEFINE A COLOR FOR THIS FRAGMENT
-  float x = cos(uTime);
-  float y = sin(uTime);
-  float t = x*vPos.x + y*vPos.y - 0.36;
+  float x = cos(uTime)*vPos.x;
+  float y = sin(uTime)*vPos.y;
+  float y2 = cos(uTime)*vPos.y;
+  float t = x*vPos.x + y*vPos.y;
 
-  float red = sin(1.*t); 
-  float green = cos(5.*t*sqrt(uTime));
-  float blue = red *red;
+  float red = max(0., cos((vPos.y)*t*sqrt(uTime)));
+  float green = max(0., x-y);
+  float blue = max(0., y2);
 
   // R,G,B EACH RANGE FROM 0.0 TO 1.0  
   vec3 color = vec3(red, green, blue);
