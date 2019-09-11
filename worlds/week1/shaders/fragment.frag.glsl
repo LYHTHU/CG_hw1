d7fe25445b9855e40a42a9e1e57543215e9a7ffb
yunhaoli <yunhaoli.cs@gmail.com>
2019-09-11 10:25:58 -0400

    update2

--- worlds/week1/shaders/fragment.frag.glsl
+++ worlds/week1/shaders/fragment.frag.glsl
@@ -7,6 +7,21 @@ in vec3 vPos;     // -1 < vPos.x < +1
 //      vPos.z == 0
 
 out vec4 fragColor; 
+
+vec3 first() {
+  float x = cos(uTime)*vPos.x;
+  float y = sin(uTime)*vPos.y;
+  float y2 = cos(uTime)*vPos.y;
+  float t = x*vPos.x + y*vPos.y;
+
+  float red = max(0., cos((vPos.y)*t*sqrt(uTime)));
+  float green = max(0., y-x);
+  float blue = max(0., y2);
+
+  // R,G,B EACH RANGE FROM 0.0 TO 1.0  
+  vec3 color = vec3(red, green, blue);
+  return color;
+}
  
 void main() {
 
@@ -21,8 +36,10 @@ void main() {
   float green = max(0., x-y);
   float blue = max(0., y2);
 
+  vec3 color = first();
+
   // R,G,B EACH RANGE FROM 0.0 TO 1.0  
-  vec3 color = vec3(red, green, blue);
+  // vec3 color = vec3(red, green, blue);
     
   // THIS LINE OUTPUTS THE FRAGMENT COLOR
   fragColor = vec4(sqrt(color), 1.0);
